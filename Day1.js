afb40908461fbf7500c485dcfa3d2550fe9a848c
Rohan Vohra <93567979+Rohan5050@users.noreply.github.com>
2024-07-13 18:40:15 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,62 @@
+// Task 1
+
+var num=55;
+console.log(num);
+
+// Task 2
+
+let name="rohan"; 
+console.log(name);
+
+// Task 3
+
+const isloggedin= true;
+console.log(isloggedin);
+
+// Javascript is Case-Sensitive
+let Name="rohan";
+let age=21;
+let Id="Ron5050";
+let isLoggedin=true;
+let obj1 = {
+    loginid:Name ,
+    password:Id ,
+    isLoggedin:true ,
+}
+
+let hero_class = ["spiderman","batman","shaktiman"];
+
+console.log(Name);
+console.log(Id);
+console.log(isLoggedin);
+console.log(obj1);
+console.log(hero_class);
+
+// Task 4
+
+console.log(typeof(Name));
+console.log(typeof(age));
+console.log(typeof(isLoggedin));
+console.log(typeof(obj1));
+console.log(typeof(hero_class));
+
+// Task 5
+
+let callme="rohan";
+callme="ron"
+
+console.log(callme)
+
+
+// Task 6
+
+const greet ="Hello Ron";
+//greet ="hello Suresh";
+
+console.log(greet);
+
+
+
+
+
+
