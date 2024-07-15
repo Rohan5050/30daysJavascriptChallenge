f6164a7fdcd75d3fde4283614fc556e0c6accb24
Rohan Vohra <rohanvohra055@gmail.com>
2024-07-15 13:25:03 +0530

    Uploaded Day 2

---
+++ Day2.js
@@ -0,0 +1,81 @@
+// Activity 1
+
+let sum = 232+235;
+let sub = 233-221;
+let mul = 23 * 5;
+let div = 32/8;
+let rem = 35 % 5;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(rem);
+
+
+// Activity 2
+let num=5;
+sum+=num;
+console.log(sum);
+
+sum-=num;
+console.log(sum);
+
+
+// Activity 3
+
+if(sum>num) {
+    console.log(sum);
+}
+
+else {
+    console.log(num);
+}
+
+if(num<sub) {
+    console.log(sum);
+}
+
+
+let num1=55;
+let num2=32;
+let num3=55;
+
+if(num1>=num3) {
+    console.log(num1)
+}
+
+else if(num1==num3) {
+    console.log("They are Equal");
+}
+
+else if(num2<=num3) {
+    console.log("num3 is greater than num1");
+}
+
+
+// Activity 4
+
+if(num1>num2 && num3>num2) {
+    console.log("num2 is smaller than both");
+}
+
+if(num2===num3 || num1===num3) {
+    console.log("They are Equal");
+}
+
+if(num1!==num2) {
+    console.log("true");
+}
+
+
+// Activity 5
+
+
+
+let num5=-55;
+
+let result = num5>0?"true":"false";
+
+console.log(result);
+
